70931ea241f5e966433255510787a7fbb77a34cc
kuwii <kuwii.someone@gmail.com>
2024-11-19 23:04:28 +0800

    2024-11-19

---
+++ leetcode/0001~0100/0001/001.2.c
@@ -0,0 +1,35 @@
+// hash - 89.46%
+
+#define SEED 92083
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = malloc(2 * sizeof(int));
+    int* hashKey = calloc(SEED, sizeof(int));
+    int* hashValue = malloc(SEED * sizeof(int));
+    int* hashIndex = malloc(SEED * sizeof(int));
+
+    *returnSize = 2;
+
+    for (int i = 0; i != numsSize; ++i) {
+        int targetNum, targetAbs, targetHash, j;
+        int num = nums[i];
+
+        targetNum = target - num;
+        targetHash = (targetNum >= 0 ? targetNum : -targetNum) % SEED;
+        for (j = targetHash; hashKey[j] == 1 && hashValue[j] != targetNum; j = (j + 1) % SEED);
+        if (hashValue[j] == targetNum) {
+            result[0] = i;
+            result[1] = hashIndex[j];
+            return result;
+        }
+
+        targetNum = num;
+        targetHash = (targetNum >= 0 ? targetNum : -targetNum) % SEED;
+        for (j = targetHash; hashKey[j] == 1 && hashValue[j] != targetNum; j = (j + 1) % SEED);
+        hashKey[j] = 1;
+        hashValue[j] = targetNum;
+        hashIndex[j] = i;
+    }
+
+    return result;
+}
---
+++ leetcode/0001~0100/0001/001.3.c
@@ -0,0 +1,39 @@
+// sort - 100%
+
+struct Number {
+    int number;
+    int index;
+};
+
+int cmp(void* a, void* b) {
+    return (*(struct Number*)a).number - (*(struct Number*)b).number;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    struct Number* tmp = malloc(sizeof(struct Number) * numsSize);
+    int* result = malloc(2 * sizeof(int));
+
+    for (int i = 0; i != numsSize; ++i) {
+        (*(tmp + i)).index = i;
+        (*(tmp + i)).number = nums[i];
+    }
+
+    *returnSize = 2;
+    qsort(tmp, numsSize, sizeof(struct Number), cmp);
+
+    int i = 0, j = numsSize - 1;
+    int sum = (*(tmp + i)).number + (*(tmp + j)).number;
+    while (sum != target) {
+        if (sum < target) {
+            ++i;
+        } else {
+            --j;
+        }
+        sum = (*(tmp + i)).number + (*(tmp + j)).number;
+    }
+
+    result[0] = (*(tmp + i)).index;
+    result[1] = (*(tmp + j)).index;
+
+    return result;
+}
